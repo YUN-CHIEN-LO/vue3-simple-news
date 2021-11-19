2c8343a60dd08604220f651f50a8e42e9dc418ad
chien.lo <yunchienlo@gmail.com>
2021-11-19 17:42:20 +0800

    [Patch] 配置部署

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  lintOnSave: false,
+  // 不希望打包後的檔名出現雜湊值
+  filenameHashing: false,
+  // 部屬
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/vue3-simple-news/" : "/",
   css: {
     loaderOptions: {
       sass: {
